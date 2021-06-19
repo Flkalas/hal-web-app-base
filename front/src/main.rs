2ff84c7724f0e080abe8ab9de27e1749c49fa64e
Flkalas <rectifying@gmail.com>
2021-06-20 00:23:17 +0900

    base: test Yew router

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,51 +1,99 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-enum Msg {
-    AddOne,
+mod pages;
+use pages::index_one::IndexOne;
+
+#[derive(Switch, Debug, Clone)]
+pub enum AppRoute {
+    #[to = "/1/{id}"]
+    One(u32),
+    #[to = "/2"]
+    Two,
+    #[to = "/"]
+    Index,
 }
 
-struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
+pub enum Msg {
+    ToggleNavbar,
+}
+
+pub struct Model {
     link: ComponentLink<Self>,
-    value: i64,
 }
+// #[derive(Switch, Debug, Clone)]
+// pub enum ForumRoute {
+//     #[to = "/{subforum}/{thread_slug}"]
+//     SubForumAndThread{subforum: String, thread_slug: String}
+//     #[to = "/{subforum}"]
+//     SubForum{subforum: String}
+// }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            value: 0,
-        }
+        Self { link }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
+            Msg::ToggleNavbar => true,
         }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
         html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <>
+                { self.view_nav() }
+
+                <main>
+                    <Router<AppRoute, ()> render=Router::render(switch) />
+                </main>
+            </>
+        }
+    }
+}
+
+impl Model {
+    fn view_nav(&self) -> Html {
+        let Self { ref link, .. } = *self;
+
+        html! {
+            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div>
+                    <div class="navbar-start">
+                        <RouterAnchor<AppRoute> classes=classes!("navbar-item").to_string() route=AppRoute::One(12)>
+                            { "One" }
+                        </RouterAnchor<AppRoute>>
+                        <RouterAnchor<AppRoute> classes=classes!("navbar-item").to_string() route=AppRoute::Two>
+                            { "Two" }
+                        </RouterAnchor<AppRoute>>
+                        <RouterAnchor<AppRoute> classes=classes!("navbar-item").to_string() route=AppRoute::Index>
+                            { "Index" }
+                        </RouterAnchor<AppRoute>>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+}
+
+fn switch(routes: &AppRoute) -> Html {
+    match routes {
+        AppRoute::One(id) => {
+            html! { <p>{"one"} {id}</p> }
+        }
+        AppRoute::Two => {
+            html! { <p>{"two"}</p> }
+        }
+        AppRoute::Index => {
+            html! { <IndexOne/> }
         }
     }
 }
